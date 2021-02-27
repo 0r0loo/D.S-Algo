5a30cebe2de7c760be9366241169f82e7025b8ec
chotaegyu <chotg5592@gmail.com>
2021-02-27 23:29:30 +0900

    21.02.27 토요일 챕터02 ~ing

---
+++ Python/doit_da_algo/c02-1/list_tuple.py
@@ -0,0 +1,32 @@
+list01 = []
+list02 = [1, 2, 3]
+list03 = ['A', 'B', 'C']
+list04 = list()
+list05 = list('ABC')  # list03이랑 같음
+list06 = list([1, 2, 3])  # [1,2,3]
+list07 = list((1, 2, 3))  # [1,2,3]
+list08 = list({1, 2, 3})  # [1,2,3]
+list09 = list(range(7))
+list10 = list(range(3, 8))  # [3,4,5,6,7]
+list11 = list(range(3, 13, 2))  # [3,5,7,9,11]
+list12 = [None] * 5
+
+
+#  -------
+tuple01 = ()
+tuple02 = 1,
+tuple03 = (1,)
+tuple04 = 1, 2, 3
+tuple05 = 1, 2, 3,
+tuple06 = (1, 2, 3)
+tuple07 = (1, 2, 3,)
+tuple08 = 'A', 'B', 'C'
+
+# 02, 03 뒤에 반드시 , 넣어줘야함 안그러면 단일 값으로 생각함
+tuple09 = tuple()
+tuple10 = tuple('ABC')
+tuple11 = tuple([1, 2, 3])
+tuple12 = tuple({1, 2, 3})
+tuple13 = tuple(range(7))
+tuple14 = tuple(range(3, 8))
+tuple15 = tuple(range(3, 13, 2))
