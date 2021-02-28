d9da180d66cf305925907f0602ca2c87f990f9f6
chotaegyu <chotg5592@gmail.com>
2021-02-28 16:40:39 +0900

    js200 16~21 done

---
+++ JavaScript/js200/part2/017.js
@@ -0,0 +1,15 @@
+// 초급 017 문자형 이해하기
+console.log('I `m in jeju');
+console.log('Sewha ocean is wonderful');
+console.log(`Have you ever eaten Makeolli?`);
+console.log('This is the firest line \nAnd this is the second')
+
+/*
+    문자열로 표현할 때는 큰 따옴표, 작은 따옴표, 억음 부호 와 함꼐 사용한다.
+    처음과 끝에 기호로 둘러싸인 형태로 문자열이 작성되며, 처음과 끝의 기호는 동일해야 한다.
+
+    다른 언어와 달리 자바스크립트는 큰 따옴표 문자열과 작은 따옴표 문자열 간의 차이점은 없다.
+    따옴표 기호들은 단일 문장이어야 유효하나, 간혹 개행이 필요한 문장도 있다.
+    이 때, 따옴표로 묶인 텍스트 안에  \n 을 추가하면 개행이 가능하다. 백 슬래쉬 뒤의 문자는 경우에 따라 변경할 수 있다.
+    
+*/
