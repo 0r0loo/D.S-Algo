a4d893c4960b868c67787cca7cee4084f6b52699
chotaegyu <chotg5592@gmail.com>
2021-03-01 12:54:19 +0900

    21 03 01 codeup 10~15 done

---
+++ Python/codeup100py/6011.py
@@ -0,0 +1,4 @@
+# 6011 : [기초-입출력] 실수 1개 입력받아 그대로 출력하기(설명)(py)
+
+floatNum = float(input())
+print(floatNum)
---
+++ Python/codeup100py/6013.py
@@ -0,0 +1,7 @@
+# 6013 : [기초-입출력] 문자 2개 입력받아 순서 바꿔 출력하기1(py)
+
+str1 = input()
+str2 = input()
+
+print(str2)
+print(str1)
